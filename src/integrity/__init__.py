1f5d904e32acacdc2477cae333381f0ba46ea013
Swervin’ Curvin (C.M.) <cmiller9851@gmail.com>
2025-12-11 10:22:02 -0500

    Add __init__.py to src/integrity to define Python package

---
+++ src/integrity/__init__.py
@@ -0,0 +1,8 @@
+# ========================================================================
+# CRA Global Integrity Engine - Integrity Package Initialization
+# Author: Cory Miller
+# License: Apache 2.0
+# Year: 2025
+# ========================================================================
+
+# This file marks the integrity directory as a Python package.
